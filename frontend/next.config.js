bac1c7c141fba775be146bcfea1520dc0adfa25b
Adam Mohamed <adammohamed@Adams-MacBook-Pro-4.local>
2025-05-18 17:26:32 -0400

    Website hosting update

---
+++ frontend/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // Enable static exports
+  experimental: {
+    isrMemoryCacheSize: 0,
+  },
+}
+
+module.exports = nextConfig 
